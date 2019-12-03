5a59b3a696f04a9d4eb67289cec535c7315e80f9
Vincent Oosterwijk <vincent@today.design>
2019-12-03 22:41:03 +1100

    Add FireMap

---
+++ packages/maps/FireMap/index.js
@@ -0,0 +1,11 @@
+import FireMap from "./FireMap.vue";
+
+const install = Vue => {
+  Vue.component("FireMap", FireMap);
+};
+
+export default {
+  install
+};
+
+export { FireMap };
--- packages/maps/index.js
+++ packages/maps/index.js
@@ -1,5 +1,7 @@
 import TideVicFreeWifiMap from './TideVicFreeWifiMap'
 import TideMapPreventionFamilyViolence from './TideMapPreventionFamilyViolence'
+import FireMap from "./FireMap";
 
 export { TideVicFreeWifiMap }
 export { TideMapPreventionFamilyViolence }
+export { FireMap }
