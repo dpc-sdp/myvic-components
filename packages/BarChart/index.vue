9f1f807fb0d9f6a9f764578799b2d06023179b48
Harry Gaitanis <harry.gaitanis@geoplex.com.au>
2020-05-12 13:10:16 +1000

    switch to using vue-chartjs library

--- packages/BarChart/index.vue
+++ packages/BarChart/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="app-chart">
-    <canvas :id="id" class="chart-canvas"></canvas>
+    <inner-chart :chartData="chartData" :options="options" />
   </div>
 </template>
 
 <script>
-import Chart from 'chart.js'
+import InnerChart from './InnerChart.js'
 import _merge from 'lodash.merge'
 
 export default {
@@ -19,24 +19,23 @@ export default {
     }
   },
   components: {
+    InnerChart
   },
-  mounted () {
-    if (!this.data) return
-    let chartSettings = {
-      datasets: [
-        {
-          backgroundColor: '#00a9e0'
-        }
-      ]
-    }
+  computed: {
+    chartData: function () {
+      let chartSettings = {
+        datasets: [
+          {
+            backgroundColor: '#00a9e0'
+          }
+        ]
+      }
 
-    let chartData = _merge({}, this.data, chartSettings)
-    var ctx = document.getElementById(this.id)
-    // eslint-disable-next-line no-unused-vars
-    var myBarChart = new Chart(ctx, {
-      type: 'bar',
-      data: chartData,
-      options: {
+      let chartData = _merge({}, this.data, chartSettings)
+      return chartData
+    },
+    options: function () {
+      const options = {
         scales: {
           xAxes: [{
             gridLines: {
@@ -45,9 +44,8 @@ export default {
           }]
         }
       }
-    })
-  },
-  computed: {
+      return options
+    }
   }
 }
 </script>
