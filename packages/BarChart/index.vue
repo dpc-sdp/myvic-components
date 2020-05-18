16bde4fcecc637515522217bea7fd7a868b4a0a4
Harry Gaitanis <harry.gaitanis@geoplex.com.au>
2020-05-18 17:19:01 +1000

    migrate stories to mdx format

--- packages/BarChart/index.vue
+++ packages/BarChart/index.vue
@@ -13,26 +13,47 @@ import ChartDataLabels from 'chartjs-plugin-datalabels'
 import builder from './utils/buildChartOptions'
 import _merge from 'lodash.merge'
 
+/**
+ * YourvicBarChart provides a generic and configurable bar chart component
+ */
 export default {
   props: {
+    /**
+     * Optional ID of this chart
+     */
     id: {
-      type: String,
-      required: true
+      type: String
     },
+    /**
+     * Direction of bar chart: can be 'horizontal' or 'vertical'
+     */
     direction: {
       type: String,
       default: 'horizontal'
     },
+    /**
+     * Title that appears above the chart. If this is null then the title and its container will not render at all
+     */
     title: {
       type: String
     },
+    /**
+     * The data to display. Refer to the Data Format section below
+     */
     data: {
       type: Object
     },
+    /**
+     * Whether to show a legend underneath the chart
+     */
     showLegend: {
       type: Boolean,
       default: false
     },
+    /**
+     * The format of the data values. The choices are:
+     * 'normal', 'percentage', 'dollar', 'thousandDollar'
+     */
     dataFormat: {
       type: String,
       default: 'normal'
---
+++ packages/BarChart/storiesConstants.js
@@ -0,0 +1,22 @@
+import _merge from 'lodash.merge'
+
+export const barChartTemplate = `
+<div style="width: 400px">
+  <bar-chart :id="id" :title="title" :direction="direction" :data="data" :showLegend="showLegend" :dataFormat="dataFormat" />
+</div>
+`
+export const chartData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [40, 20, 12, 52, 10, 39]
+    }
+  ]
+}
+
+export const dualChartData = _merge({}, chartData)
+dualChartData.datasets.push({
+  label: 'Dataset 2',
+  data: [35, 14, 11, 42, 12, 46]
+})
