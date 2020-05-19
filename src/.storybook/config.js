34d8e817652a3c35da3bdcf15678e4a50ede2fba
Brent Schiller <bschiller@geoplex.com.au>
2020-05-19 17:36:35 +1000

    Simplified Storybook folder/group structure and added sorting configuration

--- src/.storybook/config.js
+++ src/.storybook/config.js
@@ -26,6 +26,10 @@ addParameters({
     // so we can tell if a component has the right background color.
     { name: 'light', value: '#edfafc', default: true }
   ],
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 })
 
 // Install Ripple Global plugin
