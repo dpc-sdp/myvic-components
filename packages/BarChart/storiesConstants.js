f36e9bf600fdf97ddc8cb1a1f7b530b4ab21d726
Harry Gaitanis <harry.gaitanis@geoplex.com.au>
2020-06-25 13:18:24 +1000

    docs: improve storybook docs

--- packages/BarChart/storiesConstants.js
+++ packages/BarChart/storiesConstants.js
@@ -8,7 +8,7 @@ export const barChartTemplate = `
 
 export const stackedBarChartTemplate = `
 <div style="width: 400px; height: 400px">
-  <bar-chart :title="title" :direction="direction" :data="data" :stacked="true" :showLegend="showLegend" :dataFormat="dataFormat" />
+  <bar-chart :title="title" :direction="direction" :data="data" :stacked="stacked" :showLegend="showLegend" :dataFormat="dataFormat" />
 </div>
 `
 
