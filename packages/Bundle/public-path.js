0688f573a7d0dc2453e992ec075d536da9477ff0
Brent Schiller <bschiller@geoplex.com.au>
2020-07-03 14:44:25 +1000

    refactor: use dev tag for yourvic-bundle

--- packages/Bundle/public-path.js
+++ packages/Bundle/public-path.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line camelcase,no-undef
-__webpack_public_path__ = process.env.ASSET_PATH || 'https://cdn.jsdelivr.net/npm/@dpc-sdp/yourvic-bundle/dist/'
+__webpack_public_path__ = process.env.ASSET_PATH || 'https://cdn.jsdelivr.net/npm/@dpc-sdp/yourvic-bundle@dev/dist/'
