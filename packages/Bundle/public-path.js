39efb34e7803c06c15c4ae1d857f383ae2f9b535
Brent Schiller <bschiller@geoplex.com.au>
2020-07-10 12:36:25 +1000

    fix: dynamically set webpack public path in browser based on umd entrypoint script

--- packages/Bundle/public-path.js
+++ packages/Bundle/public-path.js
@@ -1,2 +1,14 @@
+// Set Webpack public path so that dynamic imports can be found in the same directory as the main bundle entrypoint
+function getCurrentScriptPath () {
+  let currentScriptSrc = ''
+  if (document && document.currentScript) { // Most browsers
+    currentScriptSrc = document.currentScript.src
+  } else { // IE
+    let scripts = document.getElementsByTagName('script')
+    currentScriptSrc = scripts[scripts.length - 1].src
+  }
+  return currentScriptSrc.substring(0, currentScriptSrc.lastIndexOf('/')) + '/'
+}
+
 // eslint-disable-next-line camelcase,no-undef
-__webpack_public_path__ = process.env.ASSET_PATH || 'https://cdn.jsdelivr.net/npm/@dpc-sdp/yourvic-bundle@dev/dist/'
+__webpack_public_path__ = process.env.YOURVIC_ASSET_PATH || getCurrentScriptPath()
