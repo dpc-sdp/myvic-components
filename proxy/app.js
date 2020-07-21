4c357a8b8becf69b9e01d80ef11f324f71aa8eb2
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-07-21 17:20:45 +1000

    test: add tests for proxy server

--- proxy/app.js
+++ proxy/app.js
@@ -1,21 +1,20 @@
 // include dependencies
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 // proxy middleware options
 const options = {
   target: 'http://stat.data.abs.gov.au', // target host
   changeOrigin: true, // needed for virtual hosted sites
   pathRewrite: {
-    '^/abs/': '',
+    '^/abs/': ''
   }
-};
+}
 
 // create the proxy (without context)
-const absProxy = createProxyMiddleware(options);
+const absProxy = createProxyMiddleware(options)
 
-// mount `exampleProxy` in web server
-const app = express();
-app.use('/abs', absProxy);
-
-app.listen(3000);
+const app = express()
+app.get('/', (req, res) => res.send('Myvic proxy server'))
+app.use('/abs', absProxy)
+app.listen(3000)
