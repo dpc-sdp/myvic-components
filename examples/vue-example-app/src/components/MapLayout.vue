5b36e08e6e85b5085bbeab570371851ecf9467f3
Brent Schiller <bschiller@geoplex.com.au>
2020-08-02 16:51:00 +1000

    feat: added layout for map address search in example site

---
+++ examples/vue-example-app/src/components/MapLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="myvic-address-search-map-layout">
+    <address-search-map />
+  </div>
+</template>
+
+<script>
+  import { AddressSearchMap } from '../../../../packages/AddressSearchMap'
+
+  export default {
+    name: 'MapLayout',
+    components: {
+      AddressSearchMap
+    },
+    data: () => {
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  @import "~@dpc-sdp/ripple-global/scss/settings";
+  @import "~@dpc-sdp/ripple-global/scss/tools";
+
+  .myvic-address-search-map-layout {
+    width: 100%;
+    height: 100%;
+  }
+</style>
