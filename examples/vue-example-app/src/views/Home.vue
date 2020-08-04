55e0499227a7bcf43469a3870fc6b413ee916641
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-08-04 19:11:25 +1000

    fix: add example back

---
+++ examples/vue-example-app/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <map-layout />
+  </div>
+</template>
+
+<script>
+import MapLayout from './../components/MapLayout.vue'
+
+export default {
+  components: {
+    MapLayout
+  }
+}
+</script>
+
+<style lang="scss">
+// Import global style in app level.
+@import "~@dpc-sdp/ripple-global/style";
+</style>
