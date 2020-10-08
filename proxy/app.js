ba9d8be7356a34905fcc51e413d27c55344c7b18
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-10-08 13:49:52 +1100

    Added proxy for address api

--- proxy/app.js
+++ proxy/app.js
@@ -3,18 +3,33 @@ const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 // proxy middleware options
-const options = {
-  target: 'http://stat.data.abs.gov.au', // target host
-  changeOrigin: true, // needed for virtual hosted sites
-  pathRewrite: {
-    '^/abs/': ''
+const items = {
+  abs: {
+    target: 'http://stat.data.abs.gov.au', // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    pathRewrite: {
+      '^/abs/': ''
+    }
+  },
+  address: {
+    target: 'https://api.vic.gov.au:443/delwp/address/v1/suggestions', // target host
+    changeOrigin: true, // needed for virtual hosted sites
+    pathRewrite: {
+      '^/api-address-suggestions': ''
+    },
+    headers: {
+      apiKey: process.env.ADDRESS_API_KEY
+    }
   }
 }
 
-// create the proxy (without context)
-const absProxy = createProxyMiddleware(options)
-
 const app = express()
 app.get('/', (req, res) => res.send('Myvic proxy server'))
+
+const absProxy = createProxyMiddleware(items.abs)
 app.use('/abs', absProxy)
+
+const addressProxy = createProxyMiddleware(items.address)
+app.use('/api-address-suggestions', addressProxy)
+
 app.listen(3000)
