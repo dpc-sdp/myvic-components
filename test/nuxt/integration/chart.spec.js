ce90f677a3771acce47a58a3de761e191e673bb8
Harry Gaitanis <harry.gaitanis@geoplex.com.au>
2021-05-28 15:46:47 +1000

    dummy commit

--- test/nuxt/integration/chart.spec.js
+++ test/nuxt/integration/chart.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Visits chart page', () => {
+describe('Visit chart page', () => {
   it('Should get bar chart', () => {
     cy.visit('/charts/bar-chart')
     cy.get('.myvic-bar-chart', { timeout: 15000 }).should('exist')
