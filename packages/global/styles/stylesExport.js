aa2ab9db81d0ef1122a64607613bfdb717451ea1
Harry Gaitanis <harry.gaitanis@geoplex.com.au>
2021-05-31 14:56:35 +1000

    add css-loader dependency to fix nuxt tests and fix issue with sass exports

---
+++ packages/global/styles/stylesExport.js
@@ -0,0 +1,66 @@
+
+export default {
+	// Export variables to make them accessible to JS
+
+	// Global
+	titleColor: '#011a3c',
+	textColor: '#53565a',
+
+	// Charts
+	legendLabelColor: '#333333',
+	gridLineColor: '#53565a80',
+	gridLineColorLight: '#53565a40',
+	gridLabelColor: '#53565a',
+	tooltipBorder: '#53565a88',
+	tooltipText: '#011a3c',
+
+	fillDefault: '#00A9E0',
+	fillDefaultHover: '#19C2F9',
+	fillSecondary: '#003866',
+	fillSecondaryHover: '#045495',
+	fillTertiary: '#97999C',
+	fillTertiaryHover: '#b3b3b3',
+	fillQuaternary: '#8DD0EC',
+	fillQuaternaryHover: '#98daf0',
+
+	strokeLine1: '#007ea7',
+	strokeLine1Hover: '#00a4db',
+	strokeLine1HoverLight: '#00a4db33',
+	strokeLine2: '#aa0031',
+	strokeLine2Hover: '#db003e',
+	strokeLine2HoverLight: '#db003e33',
+	strokeLine3: '#c75300',
+	strokeLine3Hover: '#fa6800',
+	strokeLine3HoverLight: '#fa680033',
+	strokeLine4: '#003866',
+	strokeLine4Hover: '#005499',
+	strokeLine4HoverLight: '#00549933',
+
+	treeMapFillDefault: '#b2e6f6',
+	treeMapFillDefaultHover: '#19C2F9',
+
+	fill1: '#00A9E0',
+	fill2: '#003866',
+	fill3: '#6C6F72',
+	fill4: '#c63663',
+	fill5: '#00818b',
+	fill6: '#c94000',
+	fill7: '#007b4b',
+	fill8: '#cb003b',
+	fill9: '#006e91',
+	fill10: '#9f0025',
+	fill11: '#53565a',
+	fill12: '#3b5e0f',
+	fillHover1: '#0087b3',
+	fillHover2: '#002747',
+	fillHover3: '#53565a',
+	fillHover4: '#b7274e',
+	fillHover5: '#006571',
+	fillHover6: '#d44600',
+	fillHover7: '#005e33',
+	fillHover8: '#aa0031',
+	fillHover9: '#00546f',
+	fillHover10: '#74001b',
+	fillHover11: '#45474b',
+	fillHover12: '#233809'
+}
