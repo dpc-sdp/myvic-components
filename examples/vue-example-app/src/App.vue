49411569509713b9bb3d62e4d0dcf3514dda206c
hgaitanis <30610577+hgaitanis@users.noreply.github.com>
2021-06-24 03:30:55 +0000

    fix: restore example site (#166)
    * apply changes from old PR#90
    
    * update proxy URL
    
    * move demo and home views to pages. replace package-lock.json with yarn.lock

--- examples/vue-example-app/src/App.vue
+++ examples/vue-example-app/src/App.vue
@@ -1,24 +1,5 @@
 <template>
   <div id="app">
-    <map-layout />
+    <router-view/>
   </div>
 </template>
-
-<script>
-import MapLayout from './components/MapLayout.vue'
-
-export default {
-  name: 'app',
-  components: {
-    MapLayout
-  }
-}
-</script>
-
-<style lang="scss">
-// Import global style in app level.
-@import "~@dpc-sdp/ripple-global/style";
-
-#app {
-}
-</style>
